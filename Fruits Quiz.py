8216340346dc2e27ffbdab59d338e1955fb15cae
KrithikBavanendiran <bvino1522@gmail.com>
2025-06-11 18:36:19 +0100

    Lesson 32

--- Fruits Quiz.py
+++ Fruits Quiz.py
@@ -2,16 +2,16 @@ import random
 
 class FruitQuiz:
 
-	# Create a constructor
+	
 	def __init__(self):
 		
-		# Create a dictionary of fruits as keys and color as value
+		
 		self.fruits={'apple':'red',
 					'orange':'orange',
 					'watermelon':'green',
 					'banana':'yellow'}
 
-	# Function for the quiz, here a fruit will be chosen randomly	
+	
 	def quiz(self):
 		while (True):
 			
